2f547d5af1f3664d56219f57d1e81a1e3b30360c
Batuhan Demir <batuhandemir2005@gmail.com>
2024-12-14 16:17:08 +0100

    polish language support

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT
 
 // i18n configuration
 i18n.configure({
-    locales: ['en', 'tr', 'es'],
+    locales: ['en', 'tr', 'es', 'pl'],
     directory: __dirname + '/locales',
     defaultLocale: 'en',
     queryParameter: 'lang',
