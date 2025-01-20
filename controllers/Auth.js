4118abfd86cef194617a044f78c229876381b04b
Batuhan Demir <batuhandemir2005@gmail.com>
2025-01-20 14:48:01 +0100

    cookie problem fix

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -109,7 +109,7 @@ exports.login = async (req, res) => {
             res.cookie('token', token, {
                 sameSite: process.env.PRODUCTION === 'true' ? "None" : 'Lax',
                 maxAge: new Date(Date.now() + (parseInt(process.env.COOKIE_EXPIRATION_DAYS * 24 * 60 * 60 * 1000))),
-                httpOnly: true,
+                httpOnly: false,
                 secure: process.env.PRODUCTION === 'true' ? true : false
             })
             return res.status(200).json({
